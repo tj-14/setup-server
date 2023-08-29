04386b324d3e8c075972d7e60e0934ada1b31685
Tree Saengja <g.z@live.jp>
2023-08-30 00:05:28 +0700

    shebang

--- install_vll.sh
+++ install_vll.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 # modified from https://github.com/supasorn/cluster_setup
 
 set -eu
@@ -10,30 +10,29 @@ read -s -p "sudo password: " password
 
 # add v1 alias to /etc/hosts
 for i in $node_list; do
-  ip="10.0.0.$i"
-  if ! grep -q "^$ip " /etc/hosts; then
-    echo "$password" | sudo -S sh -c "echo \"$ip v${i}\" >> /etc/hosts"
-  fi
-  # remove host from ~/.ssh/config
-  sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v$i"'$/,/^$/d;/^$/d' > tmpfile && mv tmpfile ~/.ssh/config
+	ip="10.0.0.$i"
+	if ! grep -q "^$ip " /etc/hosts; then
+		echo "$password" | sudo -S sh -c "echo \"$ip v${i}\" >> /etc/hosts"
+	fi
+	# remove host from ~/.ssh/config
+	sed 's/^Host/\n&/' ~/.ssh/config | sed '/^Host '"v$i"'$/,/^$/d;/^$/d' >tmpfile && mv tmpfile ~/.ssh/config
 done
 
-# ssh-keygen if haven't done so. 
+# ssh-keygen if haven't done so.
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<n >/dev/null 2>&1
-
 
 rm ~/.ssh/known_hosts
 
 output=()
 for i in $node_list; do
-  # remove old key
-  # ssh-keygen -f /home/$user/.ssh/known_hosts -R v$i
+	# remove old key
+	# ssh-keygen -f /home/$user/.ssh/known_hosts -R v$i
 
-  # add new key
-  sshpass -p "$password" ssh-copy-id -i /home/$user/.ssh/id_rsa.pub -oStrictHostKeyChecking=accept-new $user@v$i 
-  # ssh to auto add hostkey, and return hostname
-  val=$(ssh $user@v$i -oStrictHostKeyChecking=accept-new 'hostname')
-  output+=("$i $val")
+	# add new key
+	sshpass -p "$password" ssh-copy-id -i /home/$user/.ssh/id_rsa.pub -oStrictHostKeyChecking=accept-new $user@v$i
+	# ssh to auto add hostkey, and return hostname
+	val=$(ssh $user@v$i -oStrictHostKeyChecking=accept-new 'hostname')
+	output+=("$i $val")
 done
 
 printf '%s\n' "${output[@]}"
