530a928a79f6e3a00b0b97a4e61d4144393afc0c
tree <tossaporn@vistec.ac.th>
2024-01-25 05:59:22 +0000

    update paths

--- fish/conf.d/config.vll.fish
+++ fish/conf.d/config.vll.fish
@@ -1,4 +1,4 @@
-abbr -a run "python ~/cluster_utils/tasklauncher.py"
-abbr -a rls "python ~/cluster_utils/tasklauncher.py lsgpu"
+abbr -a run "python ~/utils/cluster_utils/tasklauncher.py"
+abbr -a rls "python ~/utils/cluster_utils/tasklauncher.py lsgpu"
 abbr -a pserv "python -m http.server 8000"
 abbr -a psv "python -m http.server 8000"
